226990851265a0db4742595898dbee7e2dd492ee
xadabunu <2907xadabunu@student.s19.be>
2023-02-07 14:36:01 +0100

    build: mise en place des premiers bindings

--- src/main/java/eu/epfc/anc3/model/GameFacade.java
+++ src/main/java/eu/epfc/anc3/model/GameFacade.java
@@ -1,4 +1,13 @@
 package eu.epfc.anc3.model;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.ReadOnlyBooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+
 public class GameFacade {
+
+    private final BooleanProperty isOn = new SimpleBooleanProperty(false);
+
+    public ReadOnlyBooleanProperty isOnProperty() {return isOn;}
+
 }
--- src/main/java/eu/epfc/anc3/vm/AppViewModel.java
+++ src/main/java/eu/epfc/anc3/vm/AppViewModel.java
@@ -1,4 +1,33 @@
 package eu.epfc.anc3.vm;
 
+import eu.epfc.anc3.model.GameFacade;
+import javafx.beans.property.ReadOnlyBooleanProperty;
+import javafx.beans.property.ReadOnlyStringProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class AppViewModel {
+
+    private final MenuViewModel menuViewModel;
+    private final FieldViewModel fieldViewModel;
+    private final GameFacade game = new GameFacade();
+
+    public FieldViewModel getFieldViewModel() {
+        return fieldViewModel;
+    }
+    public MenuViewModel getMenuViewModel() {
+        return menuViewModel;
+    }
+
+    public AppViewModel() {
+        menuViewModel = new MenuViewModel(game);
+        fieldViewModel = new FieldViewModel(game);
+    }
+
+    public ReadOnlyStringProperty titleProperty() {
+        return new SimpleStringProperty("OUR FARM");
+    }
+
+    public ReadOnlyBooleanProperty isGameOnProperty() {
+        return game.isOnProperty();
+    }
 }
